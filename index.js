5684b23076f9d2eefa5438d735efc94ad9d4ee16
Holden Altaffer <haltaffer@gmail.com>
2018-12-13 00:23:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return [drivers[0], drivers[1]];
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return [drivers[drivers.length - 2], drivers[drivers.length -1]];
+}
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+
+// const createFareMultiplier() = function() {
+//
+// }
+const createFareMultiplier = function(multiplier) {
+    return function(num) {
+        return num * multiplier;
+    };
+};
